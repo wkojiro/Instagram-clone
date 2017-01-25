6a8b99dd97c65dfc945db61b70626df454ab938a
Kojiro Wakabayashi <wkojiro@kojiro-no-macbook-pro.local>
2017-01-25 11:34:42 +0900

    loginview harfway

--- Instagram/AppDelegate.swift
+++ Instagram/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase //追加
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FIRApp.configure() //追加
         return true
     }
 
--- Instagram/ViewController.swift
+++ Instagram/ViewController.swift
@@ -7,19 +7,84 @@
 //
 
 import UIKit
+import ESTabBarController
+import Firebase
+import FirebaseAuth
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        setupTab()
+        
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        //currentUserがnilならログインしていない
+        
+        if FIRAuth.auth()?.currentUser == nil{
+            //ログインしていないときの処理
+            
+            //メソッドから抜けたあとに実行するという予約をする
+            DispatchQueue.main.async {
+                
+                //ViewControllerのstoryboardプロパティのinstantiateViewControllerWithIdentifierメソッドに最初に設定したStoryboad IDを与えることで該当のViewControllerを得ることができます。
+                let loginVIewController = self.storyboard?.instantiateViewController(withIdentifier: "Login")
+                
+                //そしてpresentViewControllerメソッドでモーダルで表示させます。このpresent()メソッドはviewDidAppearメソッド内で呼び出しても実行されません。DispatchQueue.main.asyncで、viewDidAppearメソッド終了後にpresent()メソッドを実行するという予約をします。viewDidAppearメソッドの最後はsuper.viewDidAppear(animated)で、これが実行されるとviewDidAppearメソッドは終了します。その直後にDispatchQueue.main.asyncの内部に記述されたものが実行されます。
+                self.present(loginVIewController!, animated: true, completion: nil)
+            }
+            
+            
+            
+        }
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    func setupTab(){
+        
+        // 画像のファイル名を指定してESTabBarControllerを作成する
+        let tabBarController: ESTabBarController! = ESTabBarController(tabIconNames: ["home","camera","setting"])
+        
+        // 背景色、選択時の色を設定する
+        tabBarController.selectedColor = UIColor(red: 1.0,green: 0.44, blue: 0.11, alpha: 1)
+        tabBarController.buttonsBackgroundColor = UIColor(red: 0.96, green: 0.91, blue: 0.87, alpha: 1)
+        
+        // 作成したESTabBarControllerを親のViewController（＝self）に追加する
+        addChildViewController(tabBarController)
+        view.addSubview(tabBarController.view)
+        tabBarController.view.frame = view.bounds
+        tabBarController.didMove(toParentViewController: self)
+        
+        // タブをタップした時に表示するViewControllerを設定する
+        let homeViewController = storyboard?.instantiateViewController(withIdentifier: "Home")
+        let settingViewController = storyboard?.instantiateViewController(withIdentifier: "Setting")
+        
+        tabBarController.setView(homeViewController, at: 0)
+        tabBarController.setView(settingViewController, at: 2)
+        
+        // 真ん中のタブはボタンとして扱う
+        tabBarController.highlightButton(at: 1)
+        tabBarController.setAction({
+            // ボタンが押されたらImageViewControllerをモーダルで表示する
+            let imageViewController = self.storyboard?.instantiateViewController(withIdentifier: "ImageSelect")
+            self.present(imageViewController!, animated: true, completion: nil)
+        }, at: 1)        
+        
+        
+    }
+    
+    
+    
 }
 
